2b54b41d3c0f2db521701c7323e563648d52a82d
amitai shmeeda <73076329+amitai007@users.noreply.github.com>
2023-05-01 13:22:56 +0300

    Update
    here you go

--- week2.6.5.group_by.py
+++ week2.6.5.group_by.py
@@ -6,14 +6,21 @@ def group_by(func, iterable):
     on each item in the input iterable, and the values are a list of items from the
     input iterable that result in the same key.
     """
-    result = {}
-    for item in iterable:
-        key = func(item)  # Apply the input function on the item to get the key
-        if key not in result:
-            result[key] = [item]  # If key not in dictionary, add a new key-value pair
-        else:
-            result[key].append(item)  # If key already in dictionary, append item to existing value
-    return result
+    return {func(item): [item] if func(item) not in result else result[func(item)].append(item) or result[func(item)] for item in iterable}
+     # for item in iterable:
+     #   key = func(item)  # Apply the input function on the item to get the key
+     #   if key not in result:
+     #       result[key] = [item]  # If key not in dictionary, add a new key-value pair
+     #   else:
+     #      result[key].append(item)  # If key already in dictionary, append item to existing value
 
 
 # endregion
+
+def main():
+    data = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+    result = group_by(len, data)
+    print(result)
+
+if __name__ == "__main__":
+    main()
