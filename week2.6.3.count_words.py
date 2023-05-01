b3e5bcc3a46236d26944d099d77fcab922e3b220
amitai shmeeda <73076329+amitai007@users.noreply.github.com>
2023-05-01 13:12:39 +0300

    Update
    1)main
    2)PEP8
    3) separate o/i

--- week2.6.3.count_words.py
+++ week2.6.3.count_words.py
@@ -1,17 +1,37 @@
 # region count words - this 2
 
 def count_words(text):
-    # Clean the text by removing all non-alphabetic characters and replacing them with spaces
+    """
+    Count the number of words in a text and their lengths.
+
+    Args: text (str): The text to count words from.
+
+    Returns:  dict: A dictionary where keys are words and values are their lengths.
+    """
+    # Clean the text
     cleaned_text = ''.join(c if c.isalpha() else ' ' for c in text)
 
-    # Split the text into words and convert them to lowercase
+    # Split the text
     words = cleaned_text.lower().split()
 
-    # Create a dictionary where the keys are the words and the values are their lengths
-    # Duplicate words will be overwritten by the last occurrence of the word
+    # Create a dictionary
     word_lengths = {word: len(word) for word in words}
 
     return word_lengths
-
-
 # endregion
+
+def main():
+    # Example
+    text = "The quick brown fox jumps over the lazy dog."
+    word_lengths = count_words(text)
+    print("Word lengths:")
+    for word, length in word_lengths.items():
+        print(f"{word}: {length}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
